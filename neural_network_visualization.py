8ec3f3ca164206e302bdf86270447cbf2dac116a
SagnikD95 <44514388+SagnikD95@users.noreply.github.com>
2019-09-30 15:44:37 -0500

    Add files via upload

---
+++ neural_network_visualization.py
@@ -0,0 +1,111 @@
+
+
+"""
+#VISUALIZE FEATURE MAPS
+
+
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import Model
+from matplotlib import pyplot
+from numpy import expand_dims
+import keras
+
+# load the model
+model = keras.applications.ResNet50()
+# redefine model to output right after the first hidden layer
+ixs = [5, 8,10, 15, 18]
+outputs = [model.layers[i].output for i in ixs]
+model = Model(inputs=model.inputs, outputs=outputs)
+model.summary()
+# load the image with the required shape
+img = load_img('ecg.png', target_size=(224, 224))
+# convert the image to an array
+img = img_to_array(img)
+# expand dimensions so that it represents a single 'sample'
+img = expand_dims(img, axis=0)
+# prepare the image (e.g. scale pixel values for the vgg)
+img = preprocess_input(img)
+# get feature map for first hidden layer
+feature_maps = model.predict(img)
+# plot the output from each block
+
+for fmap in feature_maps:
+    #square = int(math.sqrt(fmap.shape[3]))
+    square=1
+	# plot all 64 maps in a square
+    ix = 1
+    for _ in range(square):
+        for _ in range(square):
+			# specify subplot and turn of axis
+            fig, ax = pyplot.subplots()
+            ax = pyplot.subplot(square, square, ix)
+            ax.set_xticks([])
+            ax.set_yticks([])
+			# plot filter channel in grayscale
+            pyplot.imshow(fmap[0, :, :, ix-1], cmap='gray')
+            ix += 1
+    pyplot.show()
+
+
+"""
+
+"""
+#VISUALIZATION USING LIME
+
+
+import os
+import keras
+from keras.applications import inception_v3 as inc_net
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import decode_predictions
+from skimage.io import imread
+import matplotlib.pyplot as plt
+import numpy as np
+print('Notebook run using keras:', keras.__version__)
+
+
+inet_model = inc_net.InceptionV3()
+def transform_img_fn(path_list):
+    out = []
+    for img_path in path_list:
+        img = image.load_img(img_path, target_size=(299,299))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = inc_net.preprocess_input(x)
+        out.append(x)
+    return np.vstack(out)
+
+images = transform_img_fn([os.path.join('data','cat_mouse.jpg')])
+# I'm dividing by 2 and adding 0.5 because of how this Inception represents images
+plt.imshow(images[0] / 2 + 0.5)
+preds = inet_model.predict(images)
+for x in decode_predictions(preds)[0]:
+    print(x)
+    
+import os,sys
+try:
+    import lime
+except:
+    sys.path.append(os.path.join('..', '..')) # add the current directory
+    import lime
+from lime import lime_image
+
+explainer = lime_image.LimeImageExplainer()
+
+# Hide color is the color for a superpixel turned OFF. Alternatively, if it is NONE, the superpixel will be replaced by the average of its pixels
+explanation = explainer.explain_instance(images[0], inet_model.predict, top_labels=5, hide_color=0, num_samples=1000)
+from skimage.segmentation import mark_boundaries
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=True)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10, hide_rest=False)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=1000, hide_rest=False, min_weight=0.1)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+
+
+"""
